e5c73abafecbe8c2f5954345bb12b41cee45cc0d
Simon Mihoyo <109045796+SimonMihoyo@users.noreply.github.com>
2025-07-15 14:58:37 +0800

    Initial Commit

---
+++ Launcher.MC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Launcher.MC
+//
+//  Created by 四月初一茶染绮良 on 2025/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Launcher.MC/Launcher_MCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launcher_MCApp.swift
+//  Launcher.MC
+//
+//  Created by 四月初一茶染绮良 on 2025/7/15.
+//
+
+import SwiftUI
+
+@main
+struct Launcher_MCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
